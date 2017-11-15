8c2f74636d7329088d8d1131c1a757a5c6de4532
eriksuperaxel <33615045+eriksuperaxel@users.noreply.github.com>
2017-11-14 23:14:31 -0600

    Add files via upload

--- pingpong.php
+++ pingpong.php
@@ -1,9 +1,8 @@
 <!doctype html>
 <html>
 <head>
-    <title>PHP: Ping Pong Simulation Game!</title>
+    <title>Juego de simulacion de una partida de Ping Pong</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 
 </head>
 <body>
@@ -18,19 +17,10 @@
         }
 
         body {
-            background: pink;
-            font-family: "Roboto Slab";
+            backgronda: skyblue;
 
         }
 
-        header {
-
-            width: 100%;
-            height: 150px;
-            background: blue;
-            color: white;
-
-        }
 
         .pp_head {
             text-align: center;
@@ -42,14 +32,14 @@
             position: relative;
             width: 400px;
             margin: 20px auto;
-            background: yellow;
+            backgronda: yellow;
             /*padding: 15px;*/
             box-sizing: content-box;
 
         }
 
         div.rallies:nth-child(2n) {
-            background: lightgreen;
+            backgronda: lightgreen;
             color: orangered;
 
         }
@@ -58,24 +48,11 @@
             padding-left: 20px;
         }
 
-        footer {
-            text-align: center;
-            margin-top: 20px;
+        
 
-        }
+        
 
-        .logo {
-            position: relative; 
-            top: 20px;
-            margin: 0 auto;
-            display: block;
-            width: 100px;
-            height: 100px;
-            border-radius: 100%;
-
-        }
-
-        h2.winner {
+        h2.ganador {
             margin-top: 10px;
             padding: 10px;
             text-align: center;
@@ -92,12 +69,9 @@
 
 
     <?php
-
-
-        //initialise and declare variables for player and round scores.
-        $player1 = 0;
-        $player2 = 0;
-        $round = 0;
+        $jugador1 = 0;
+        $jugador2 = 0;
+        $ronda = 0;
     ?>
 
 
@@ -105,61 +79,51 @@
 
 
 
-    <header>
-        <img class="logo" src = "logo.png" />
-    </header>
 
     <div class="pp_head">
-        <h1>PHP Ping Pong Simulation Game</h1>
+        <h1>Ping Pong</h1>
 
-        <p> Let's play!</p>
+        <p>Inicia el juego!</p>
     </div>
 
     <div class = "content">
 
-        <div class="round">
+        <div class="ronda">
             <?php
 
-                //WIN
-                //player must reach a score of 11
-                //player must be a minimum of 2 point higher than opponent
-
-                //continue the game while the absolute value is less than ;
-                while (abs($player1 - $player2) < 2 || $player1 < 11 && $player2 < 11) {
-                   $round++;
+                //para ganar un jugador debe llegar a 11 puntos minimo ademas de ganar por diferencia de 2 o mas puntos o sino debera seguir el juego
+                while (abs($jugador1 - $jugador2) < 2 || $jugador1 < 11 && $jugador2 < 11) {
+                   $ronda++;
                    echo "<div class =\"rallies\">";
-                   echo "<h2>Round $round</h2>\n";
+                   echo "<h2>Ronda $ronda</h2>\n";
 
-                   //random scores between 1 (max) and 0 (min)
+                   //resultados al azar 1 maximo y 0 minimo
                    if(rand(0,1)) {
-                     $player1++;
+                     $jugador1++;
 
                    } else {
-                     $player2++;
+                     $jugador2++;
                    }
-                   echo "Player1 = $player1<br />\n";
-                   echo "Player2 = $player2<br />\n";
+                   echo "Jugador1 = $jugador1<br />\n";
+                   echo "Jugador2 = $jugador2<br />\n";
                    echo "</div>";
                 }
             ?>
         </div>
 
-        <div class = "winner">
+        <div class = "ganador">
             <?php
-                if($player1 > $player2) {
-                   echo "<h2 class=\"winner\">Player 1";
+                if($jugador1 > $jugador2) {
+                   echo "<h2 class=\"ganador\">Jugador 1";
                 } else {
-                   echo "<h2 class=\"winner\">Player 2";
+                   echo "<h2 class=\"ganador\">Jugador 2";
                 }
 
-                echo " is the winner after $round rounds</h2>"; 
+                echo " es el ganador despues de $ronda rondas</h2>"; 
             ?>
         </div>
      </div>
 
-    <footer>
-        <p>PHP Ping Pong Simulation Game. From Teehouse by Jonnie Grieve Digital Media</a></p> 
-    </footer>
 </body>
 
 </html>
